9897c5fdce2ba476f7789e58924b399ecdf3b6ea
EllenMacedo <112514908+EllenMacedo@users.noreply.github.com>
2023-07-03 21:17:04 -0300

    Add files via upload

---
+++ TabelaHash.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <string.h>
+#include "TabelaHash.h"
+
+//definição do tipo hash
+struct hash {
+    int qtd, TABLE_SIZE;
+    struct aluno **itens;
+};
+
+Hash* criaHash(int TABLE_SIZE){
+    Hash *ha;
+    ha = (Hash*) malloc(sizeof(Hash));
+    if (ha != NULL) {
+    int i;
+    ha->TABLE_SIZE = TABLE_SIZE;
+    ha->itens = (struct aluno**)malloc(TABLE_SIZE * sizeof(struct aluno*));
+    if (ha->itens == NULL) {
+    free(ha);
+    return NULL;
+    }
+    ha->qtd = 0;
+    for (i=0; i < ha->TABLE_SIZE; i++)
+    ha->itens[i] = NULL;
+    }
+    return ha;
+}
+
+void liberaHash(Hash* ha){
+    if (ha != NULL) {
+    int i;
+    for (i=0; i < ha->TABLE_SIZE; i++) {
+    if (ha->itens[i] != NULL)
+    free(ha->itens[i]);
+    }
+    free(ha->itens);
+    free(ha);
+    }
+}
+int insereHash_SemColisao(Hash* ha, struct aluno al){
+if (ha == NULL || ha->qtd == ha->TABLE_SIZE)
+return 0;
+int chave = al.matricula;
+//int chave = valorString(al.nome);
+int pos = chaveDivisao(chave, ha->TABLE_SIZE);
+struct aluno *novo;
+novo = (struct aluno*)malloc(sizeof(struct aluno));
+if (novo == NULL)
+return 0;
+*novo = al;
+ha->itens[pos] = novo;
+ha->qtd++;
+return 1;
+}
+int buscaHash_SemColisao(Hash* ha, int mat, struct aluno *al){
+if (ha == NULL)
+return 0;
+int pos = chaveDivisao(mat, ha->TABLE_SIZE);
+if (ha->itens[pos] == NULL)
+return 0;
+*al = *(ha->itens[pos]);
+return 1;
+}
+
--- main.c
+++ main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TabelaHash.h"
 
+int main(){
+    aluno al;
+
+    printf("Matricula:\n");
+    scanf("%d", &al.matricula);
+    printf("Nome:\n");
+    scanf("%s", &al.nome);
+    printf("Nota 1:\n");
+    scanf("%f", &al.n1);
+
+    Hash *ha = criaHash(1427);
+
+    int x=insereHash_SemColisao(ha, al);
+    int x = buscaHash_SemColisao(ha, mat, &al);
+
+    liberaHash(ha);
+    return 0;
+
+}
